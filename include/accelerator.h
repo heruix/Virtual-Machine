03fcf1004f87d8383bc47cce18e8fdaa8820153e
Tyrion Lannister <Tyrion.Lannister.274AL@gmail.com>
2014-10-07 12:34:49 +0800

    diff --git a/include/accelerator.h b/include/accelerator.h index e69de29..0305464 100644 --- a/include/accelerator.h +++ b/include/accelerator.h @@ -0,0 +1,19 @@ +#ifndef ACCELERATOR_H +#define ACCELERATOR_H + +#include <stdlib.h> +#include <stdarg.h> + +typedef void* (VCPUThread)(void *data); + +typedef struct { +    size_t size; +    void *(*ctor)(void *_self, va_list *_params); +    void *(*dtor)(void *_self); +    void *(*init)(void *_self); +    void (*createVM)(void *_self, int _ramSize); +    void (*initVCPU)(void *_self, VCPUThread _thread); +    void (*run)(void *_self); +} Accelerator; + +#endif

---
+++ include/accelerator.h
@@ -0,0 +1,19 @@
+#ifndef ACCELERATOR_H
+#define ACCELERATOR_H
+
+#include <stdlib.h>
+#include <stdarg.h>
+
+typedef void* (VCPUThread)(void *data);
+
+typedef struct {
+    size_t size;
+    void *(*ctor)(void *_self, va_list *_params);
+    void *(*dtor)(void *_self);
+    void *(*init)(void *_self);
+    void (*createVM)(void *_self, int _ramSize);
+    void (*initVCPU)(void *_self, VCPUThread _thread);
+    void (*run)(void *_self);
+} Accelerator;
+
+#endif
